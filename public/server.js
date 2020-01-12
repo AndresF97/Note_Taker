baacaacd1ff3727f80cdb0e5dd4edf4dcbd526cc
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-11 23:45:35 -0800

    added an empty ob ject to ad the notes from the user

--- public/server.js
+++ public/server.js
@@ -1,8 +1,15 @@
+//Calling for npm and setting up 
 const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 3000;
 app.use(express.urlencoded({extended : true}))
+
+var notes = [
+    {
+        
+    }
+]
 
 
 app.get('/note',function(req,res){
