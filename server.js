c81459c884850385719074e1e1fa20e15583b2c1
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-13 22:57:45 -0800

    move code round in the server.js file and worked on the delete button with the app.get(/api/notes/:id)

--- server.js
+++ server.js
@@ -7,30 +7,18 @@ const  PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(express.static("public"))
-
-
-
-app.get("/api/notes",function(req,res){
-    fs.readFile("./db/db.json",function(err,data){
-        if(err) throw err
-        return  res.json(JSON.parse(data))
-    })
-})
-app.get('/note',function(req,res){
-    res.sendFile(path.join(__dirname,"public/notes.html"))
-})
-app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname,"public/index.html"))
-})
-
+let note = []
 app.post("/api/notes",function(req,res){
     var newNotes = req.body;
     console.log(newNotes)
     fs.readFile("./db/db.json","utf8",(err,data)=>{
         if(err) throw err
-        var note =[] 
+        
         note.push(JSON.parse(data));
         note.push(newNotes);
+        note.forEach((item,i) => {
+            item.id = 1 + i
+        })
         fs.writeFile("./db/db.json",JSON.stringify(note),(err)=>{
             if(err) throw err
         })
@@ -39,9 +27,50 @@ app.post("/api/notes",function(req,res){
 
 });
 
-app.get("api/notes/:id",function(req,res){
-    
+app.get("/api/notes",function(req,res){
+    fs.readFile("./db/db.json",function(err,data){
+        if(err) throw err
+        noter = (JSON.parse(data))
+        return  res.json(JSON.parse(data))
+    })
 })
+app.get("/api/notes/:id",function(req,res){
+    var chosen = req.params.id;
+        console.log(chosen)
+        for(var i = 0 ; i < note.length;i++){
+        if(chosen === note[i].id){
+            return res.json(note[i])
+        }
+    }
+        return res.json(false)
+})
+app.get('/note',function(req,res){
+    res.sendFile(path.join(__dirname,"public/notes.html"))
+})
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,"public/index.html"))
+})
+
+// app.post("/api/notes",function(req,res){
+//     var newNotes = req.body;
+//     console.log(newNotes)
+//     fs.readFile("./db/db.json","utf8",(err,data)=>{
+//         if(err) throw err
+        
+//         note.push(JSON.parse(data));
+//         note.push(newNotes);
+//         note.forEach((item,i) => {
+//             item.id = 1 + i
+//         })
+//         fs.writeFile("./db/db.json",JSON.stringify(note),(err)=>{
+//             if(err) throw err
+//         })
+//     })
+//     res.json(newNotes)
+
+// });
+
+
 
 
 
